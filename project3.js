d5d279b8ba84c777c729569f5021d7e77d206adf
imani-elliott23 <imani.elliott23@outlook.com>
2024-07-17 20:22:45 -0400

    Update project3.js

--- project3.js
+++ project3.js
@@ -6,15 +6,15 @@ let highScore = 0;
 
 document.getElementById("checkButton").addEventListener("click", function() {
     const guess = parseInt(document.getElementById("guessInput").value);
-  
-    if (guess === targetNumber) {
+      if (isNaN(guess) || guess < 1 || guess > 100) {
+        document.getElementById('message').innerText = 'Please enter a valid number between 1 and 100.';  
+          if (guess === targetNumber) {
         alert("Congratulations! You guessed the correct number!");
         score++;
         if (score > highScore) {
             highScore = score;
             document.getElementById("highScore").textContent = highScore;
-                if (isNaN(guess) || guess < 1 || guess > 100) {
-        document.getElementById('message').innerText = 'Please enter a valid number between 1 and 100.';
+                
         }
     } else {
         alert("Sorry, that's not the correct number. Try again!");
